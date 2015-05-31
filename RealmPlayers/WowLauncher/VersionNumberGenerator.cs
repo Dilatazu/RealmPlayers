8d39ab714c3345108c2f8faaf3cd17c8032a1534
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-06-01 00:40:49 +0200

    nothing special...

--- RealmPlayers/WowLauncher/VersionNumberGenerator.cs
+++ RealmPlayers/WowLauncher/VersionNumberGenerator.cs
@@ -4,5 +4,5 @@
 // 
 
 using System.Reflection;
-[assembly: AssemblyVersion("1.3.6.122")]
-[assembly: AssemblyFileVersion("1.3.6.122")]
+[assembly: AssemblyVersion("1.3.6.150")]
+[assembly: AssemblyFileVersion("1.3.6.150")]
