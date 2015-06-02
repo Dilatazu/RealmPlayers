de0d021e7cf0d3363f302848499a4cf91991ce57
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-06-02 22:19:55 +0200

    [RaidStats] Fixed so that BossList uses same interface as FightOverall for highscore. Also added support for filtering by playername

--- RealmPlayers/RaidStatsWebsite/BossList.aspx.cs
+++ RealmPlayers/RaidStatsWebsite/BossList.aspx.cs
@@ -24,12 +24,14 @@ namespace VF.RaidDamageWebsite
         protected void Page_Load(object sender, EventArgs e)
         {
             string guildLimit = PageUtility.GetQueryString(Request, "Guild", "null");
+            string playerLimit = PageUtility.GetQueryString(Request, "player", "null");
 
             var realm = RealmControl.Realm;
 
             string breadCrumbCommon = "";
             if (guildLimit != "null")
             {
+                playerLimit = "null";
                 this.Title = "Boss records for " + guildLimit + " | RaidStats";
 
                 breadCrumbCommon = PageUtility.BreadCrumb_AddHome()
@@ -39,6 +41,18 @@ namespace VF.RaidDamageWebsite
                 m_BossListInfoHTML = new MvcHtmlString("<h1>Raid Bosses for " + guildLimit + "</h1>"
                     + "<p>Top DPS, HPS and fastest kill times are listed below for the different bosses</p>"
                     + "<p>Note that this does only show data from the guild " + guildLimit + ".<br />If you want to see for all guilds click " + PageUtility.CreateLink("BossList.aspx?Realm=" + RealmPlayersServer.StaticValues.ConvertRealmParam(realm), "here") + "</p>");
+            }
+            else if(playerLimit != "null")
+            {
+                this.Title = "Boss records for " + playerLimit + " | RaidStats";
+
+                breadCrumbCommon = PageUtility.BreadCrumb_AddHome()
+                    + PageUtility.BreadCrumb_AddRealm(realm)
+                    + PageUtility.BreadCrumb_AddLink("PlayerOverview.aspx?realm=" + RealmPlayersServer.StaticValues.ConvertRealmParam(realm) + "&player=" + playerLimit, playerLimit);
+
+                m_BossListInfoHTML = new MvcHtmlString("<h1>Raid Boss records for " + playerLimit + "</h1>"
+                    + "<p>Top DPS, HPS and fastest kill times are listed below for the different bosses</p>"
+                    + "<p>Note that this does only show data for player " + playerLimit + ".<br />If you want to see for all players on the realm click " + PageUtility.CreateLink("BossList.aspx?Realm=" + RealmPlayersServer.StaticValues.ConvertRealmParam(realm), "here") + "</p>");
             }
             else
             {
@@ -105,38 +119,10 @@ namespace VF.RaidDamageWebsite
             string tableBody = "";
             foreach (var boss in BossInformation.BossFights)
             {
-                List<VF_RDDatabase.BossFight> bossFights = new List<VF_RDDatabase.BossFight>();
-                double averagePrecision = 0.0;
-                foreach (var groupRC in summaryDatabase.GroupRCs)
-                {
-                    if (realm != VF_RealmPlayersDatabase.WowRealm.All && realm != groupRC.Value.Realm)
-                        continue;
-
-                    if (guildLimit != "null" && guildLimit != groupRC.Value.GroupName)
-                        continue;
-                    foreach (var raid in groupRC.Value.Raids)
-                    {
-                        if (raid.Value.RaidInstance == boss.Value)
-                        {
-                            //if (realmDB.Realm != raid.Value.CacheGroup.Realm)
-                            //    realmDB = ApplicationInstance.Instance.GetRealmDB(raid.Value.CacheGroup.Realm);
-                            //var fights = raid.Value.BossFightsGetBossFights(ApplicationInstance.Instance.GetRaidFightCollection);
-                            foreach (var bossFight in raid.Value.BossFights)
-                            {
-                                if (bossFight.BossName == boss.Key
-                                && bossFight.StartDateTime > earliestCompatibleDate
-                                && bossFight.IsQualityHigh())
-                                {
-                                    averagePrecision += bossFight.DataDetails.FightPrecision;// fight.CalculatePrecision(realmDB.RD_IsPlayer);
-                                    bossFights.Add(bossFight);
-                                }
-                            }
-                        }
-                    }
-                }
+                var bossFights = summaryDatabase.GetHSElligibleBossFights(boss.Key, guildLimit == "null" ? null : guildLimit, playerLimit == "null" ? null : playerLimit);
                 if (bossFights.Count > 0)
                 {
-                    averagePrecision /= bossFights.Count;
+                    //averagePrecision /= bossFights.Count;
                     Tuple<string, int> fastestKill = new Tuple<string, int>("", int.MaxValue);
                     Tuple<string, double> topDPS = new Tuple<string, double>("", 0.0);
                     Tuple<string, double> topHPS = new Tuple<string, double>("", 0.0);
@@ -144,8 +130,8 @@ namespace VF.RaidDamageWebsite
                     {
                         if (realmDB.Realm != fight.CacheRaid.CacheGroup.Realm)
                             realmDB = ApplicationInstance.Instance.GetRealmDB(fight.CacheRaid.CacheGroup.Realm);
-                        if (fight.DataDetails.FightPrecision < averagePrecision - 0.05)
-                            continue;
+                        //if (fight.DataDetails.FightPrecision < averagePrecision - 0.05)
+                        //    continue;
                         if (fight.FightDuration < fastestKill.Item2)
                         {
                             fastestKill = new Tuple<string,int>(
@@ -164,6 +150,8 @@ namespace VF.RaidDamageWebsite
                                 var playerName = unit.Item1;
                                 if (playerName == "Unknown")
                                     continue;
+                                if (playerLimit != "null" && playerLimit != playerName)
+                                    continue;
                                 if (BossInformation.BossFights.ContainsKey(playerName) == true)
                                     continue;
 
@@ -176,6 +164,8 @@ namespace VF.RaidDamageWebsite
                             {
                                 var playerName = unit.Item1;
                                 if (playerName == "Unknown")
+                                    continue;
+                                if (playerLimit != "null" && playerLimit != playerName)
                                     continue;
                                 if (BossInformation.BossFights.ContainsKey(playerName) == true)
                                     continue;
