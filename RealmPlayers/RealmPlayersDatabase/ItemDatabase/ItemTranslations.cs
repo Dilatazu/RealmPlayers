90bd39931020730bb83fa104a94fabaaf6d57c8e
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-06-20 11:13:16 +0200

    Added ItemTranslations for Mounts and Companions!

---
+++ RealmPlayers/RealmPlayersDatabase/ItemDatabase/ItemTranslations.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VF
+{
+    class ItemTranslations
+    {
+        public Dictionary<string, int> _NameToIDTranslation = new Dictionary<string, int>
+        {
+            //Orc mounts
+            {"Arctic Wolf", 12351},
+            {"Red Wolf", 12330},
+
+            {"Brown Wolf", 5668},
+            {"Dire Wolf", 5665},
+            {"Timber Wolf", 1132},
+
+            {"Swift Timber Wolf", 18797},
+            {"Swift Gray Wolf", 18798},
+            {"Swift Brown Wolf", 18796},
+            
+            //Tauren mounts
+            {"Brown Kodo", 15290},
+            {"Gray Kodo", 15277},
+
+            {"Great Gray Kodo", 18795},
+            {"Great Brown Kodo", 18794},
+            {"Great White Kodo", 18793},
+
+            {"Green Kodo", 15292},
+            {"Teal Kodo", 15293},
+            
+            //Troll mounts
+            {"Emerald Raptor", 8588},
+            {"Turquoise Raptor", 8591},
+            {"Violet Raptor", 8592},
+
+            {"Swift Blue Raptor", 18788},
+            {"Swift Olive Raptor", 18789},
+            {"Swift Orange Raptor", 18790},
+
+            {"Ivory Raptor", 13317},
+            {"Mottled Red Raptor", 8586},
+            
+            //Undead mounts
+            {"Blue Skeletal Horse", 13332},
+            {"Brown Skeletal Horse", 13333},
+            {"Red Skeletal Horse", 13331},
+
+            {"Purple Skeletal Warhorse", 18791},
+            {"Green Skeletal Warhorse", 13334},
+            
+            //Nightelf mounts
+            {"Spotted Frostsaber", 8632},
+            {"Spotted Nightsaber", 8628},
+            {"Striped Frostsaber", 8631},
+            {"Striped Nightsaber", 8629},
+            
+            {"Frostsaber", 12302},
+            {"Nightsaber", 12303},
+
+            {"Swift Frostsaber", 8632},
+            {"Swift Stormsaber", 18902},
+            {"Swift Mistsaber", 18767},
+            
+            //Dwarf mounts
+            {"Brown Ram", 5872},
+            {"Gray Ram", 5864},
+            {"White Ram", 5873},
+
+            {"Black Ram", 13328},
+            {"Frost Ram", 13329},
+            
+            {"Swift Brown Ram", 18786},
+            {"Swift Gray Ram", 18787},
+            {"Swift White Ram", 18785},
+            
+            //Gnome mounts
+            {"Red Mechanostrider", 8563},
+            {"Green Mechanostrider", 13321},
+            {"Blue Mechanostrider", 8595},
+            {"Unpainted Mechanostrider", 13322},
+            
+            {"Icy Blue Mechanostrider", 13327},
+            {"White Mechanostrider", 13326},
+
+            {"Swift Green Mechanostrider", 18772},
+            {"Swift White Mechanostrider", 18773},
+            {"Swift Yellow Mechanostrider", 18774},
+            
+            //Human mounts
+            {"Black Stallion", 2411},
+            {"Brown Horse", 5656},
+            {"Chestnut Mare", 5655},
+            {"Pinto Horse", 2414},
+            
+            {"Swift White Stallion", 12353},
+
+            {"Swift White Steed", 18778},
+            {"Swift Brown Steed", 18777},
+            {"Swift Palomino", 18776},
+            
+            //Rare drop mounts
+            {"Winterspring Frostsaber", 13086},
+            {"Deathcharger", 13335},
+            {"Swift Spectral Tiger", 33225},
+
+            //AV mounts
+            {"Stormpike Battle Charger", 19030},
+            {"Frostwolf Howler", 19029},
+
+            //Rank11 mounts
+            {"Black War Kodo", 18247},
+            {"Black War Raptor", 18246},
+            {"Black War Wolf", 18245},
+            {"Red Skeletal Warhorse", 18248},
+
+            {"Black War Steed", 18241},
+            {"Black War Tiger", 18242},
+            {"Black War Ram", 18244},
+            
+            //AQ40 mounts
+            {"Summon Red Qiraji Battle Tank", 21321},
+            {"Summon Yellow Qiraji Battle Tank", 21324},
+            {"Summon Blue Qiraji Battle Tank", 21218},
+            {"Summon Black Qiraji Battle Tank", 21176},
+            
+            //ZG mounts
+            {"Swift Razzashi Raptor", 19872},
+            {"Swift Zulian Tiger", 19902},
+            
+            //Occassional mounts
+            {"Reindeer", 21211},
+
+            /*
+            //Warlock mounts
+            {"Summon Felsteed", 5784}, --SPELL
+            {"Summon Dreadsteed", 23161}, --SPELL
+            
+            //Paladin mounts
+            {"Summon Warhorse", 13819}, --SPELL
+            {"Summon Charger", 23214}, --SPELL
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*---------------------------------COMPANIONS---------------------------------*/
+            
+            {"Jubling", 19450},
+            {"Snowshoe Rabbit", 8497},
+            {"Cockatiel", 8496},
+            {"Disgusting Oozeling", 20769},
+
+            {"White Kitten", 8489},
+            {"Bombay Cat", 8485},
+            {"Siamese Cat", 8490},
+            {"Cornish Rex Cat", 8486},
+
+            {"Black Tabby Cat", 8491},
+            {"Silver Tabby Cat", 8488},
+            {"Orange Tabby Cat", 8487},
+
+            {"Great Horned Owl", 8500},
+            {"Green Wing Macaw", 8492},
+            {"Hyacinth Macaw", 8494},
+
+            {"Dark Whelpling", 10822},
+            {"Tiny Crimson Whelpling", 8499},
+            {"Tiny Emerald Whelpling", 8498},
+            
+            {"Ancona Chicken", 11023},
+        };
+    }
+}
