b11bb078b7375c990cca8b8adee25ee8de3a21cb
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-07-12 10:27:27 +0200

    [RaidStats] Added DungeonList.aspx

---
+++ RealmPlayers/RaidStatsWebsite/DungeonList.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RaidDamageMasterFrame.Master" AutoEventWireup="true" CodeBehind="DungeonList.aspx.cs" Inherits="VF.RaidDamageWebsite.DungeonList" %>
+
+<%@OutputCache Duration="60" VaryByParam="*" %>
+<%@ Register Src="RealmControl.ascx" TagPrefix="uc1" TagName="RealmControl" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeaderContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+    <ul class="breadcrumb">
+        <%= m_BreadCrumbHTML %>
+    </ul><!--/.breadcrumb -->
+    
+    <header class="page-header">  
+        <div class="row">
+          <div class="span8">
+              <%= m_DungeonListInfoHTML %>
+          </div>
+          <div class="span4" style="min-width:200px;">
+              <div style="margin: 0px 0px 0px 10px; float:right; ">
+                    <uc1:RealmControl runat="server" ID="RealmControl" />
+                </div>
+          </div>
+        </div>
+    </header>
+    
+    <%= m_PaginationHTML %>
+
+    <table id="characters-table" class="table">
+        <thead>
+            <%= m_TableHeadHTML %>
+        </thead>
+        <tbody>
+            <%= m_TableBodyHTML %>
+        </tbody>
+    </table>
+    
+    <%= m_PaginationHTML %>
+</asp:Content>
---
+++ RealmPlayers/RaidStatsWebsite/DungeonList.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Mvc;
+
+namespace VF.RaidDamageWebsite
+{
+    public partial class DungeonList : System.Web.UI.Page
+    {
+        public MvcHtmlString m_BreadCrumbHTML = null;
+        public MvcHtmlString m_DungeonListInfoHTML = null;
+
+        public MvcHtmlString m_TableHeadHTML = null;
+        public MvcHtmlString m_TableBodyHTML = null;
+
+        public MvcHtmlString m_PaginationHTML = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int pageNr = PageUtility.GetQueryInt(Request, "page", 1);
+            int pageIndex = pageNr - 1;//Change range from 0 to * instead of 1 to *
+            int count = PageUtility.GetQueryInt(Request, "count", 50);
+            if (count > 500) count = 500;
+
+            var realm = RealmControl.Realm;
+
+            this.Title = "Raids | RaidStats";
+
+            m_BreadCrumbHTML = new MvcHtmlString(PageUtility.BreadCrumb_AddHome()
+                + PageUtility.BreadCrumb_AddRealm(realm) + PageUtility.BreadCrumb_AddFinish("Dungeons"));
+            m_DungeonListInfoHTML = new MvcHtmlString("<h1>All recorded dungeons</h1><p>Sorted by the time the dungeon ended</p>");
+
+            var raidCollection = ApplicationInstance.Instance.GetRaidCollection();
+            var orderedDungeons = raidCollection.m_Dungeons.OrderByDescending((_Value) => { return _Value.Value.m_DungeonEndDate; });
+            int nr = 0;
+
+            int i = orderedDungeons.Count();
+            string tableBody = "";
+            foreach (var dungeon in orderedDungeons)
+            {
+                if (dungeon.Value.Realm == VF_RealmPlayersDatabase.WowRealm.Test_Server)
+                    continue;//Skip Test_Server
+                if ((realm == VF_RealmPlayersDatabase.WowRealm.All || realm == dungeon.Value.Realm))
+                {
+                    nr++;
+                    if (nr > pageIndex * count && nr <= (pageIndex + 1) * count)
+                    {
+                        string dungeonOwnerVisual = "\"" + dungeon.Value.m_GroupMembers.MergeToStringVF("\", \"") + "\"";
+
+                        tableBody += PageUtility.CreateTableRow("", PageUtility.CreateTableColumn("#" + i) +
+                            PageUtility.CreateTableColumn(dungeonOwnerVisual) +
+                            PageUtility.CreateTableColumn(PageUtility.CreateLink("DungeonOverview.aspx?Dungeon=" + dungeon.Value.m_UniqueDungeonID, dungeon.Value.m_Dungeon)) +
+                            PageUtility.CreateTableColumn(dungeon.Value.m_DungeonStartDate.ToLocalTime().ToString("yyy-MM-dd HH:mm:ss")) +
+                            PageUtility.CreateTableColumn(dungeon.Value.m_DungeonEndDate.ToLocalTime().ToString("yyy-MM-dd HH:mm:ss")) +
+                            PageUtility.CreateTableColumn(RealmPlayersServer.StaticValues.ConvertRealmViewing(dungeon.Value.Realm)));
+                    }
+                    if (nr >= (pageIndex + 1) * count)
+                        break;
+                }
+                --i;
+            }
+            if (nr != 0 && nr <= pageIndex * count)
+            {
+                pageIndex = (nr - 1) / count;
+                Response.Redirect(PageUtility.CreateUrlWithNewQueryValue(Request, "page", (pageIndex + 1).ToString()));
+            }
+            m_TableBodyHTML = new MvcHtmlString(tableBody);
+
+            m_PaginationHTML = new MvcHtmlString(PageUtility.CreatePagination(Request, pageNr, ((orderedDungeons.Count() - 1) / count) + 1));
+        }
+    }
+}
