68c2a6cc28e28cb20576e4e4629bf42e82cae572
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-10-13 21:50:12 +0200

    removed local files

--- RealmPlayers/MongoDBPorting/DistributeToServer.bat
+++
@@ -1,5 +0,0 @@
-xcopy /s /D /Y ".\bin\Release\*.exe" "\\***REMOVED***\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.pdb" "\\***REMOVED***\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.dll" "\\***REMOVED***\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.xml" "\\***REMOVED***\VF_RealmPlayersData\WowLauncher"
-PAUSE
