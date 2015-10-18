01fe616f77f15b8aa3de7d50d05c73265ae9c0e0
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-10-18 18:12:38 +0200

    Cleaned up the solution some. Removed unused old projects.

--- RealmPlayers/MongoDBPorting/DistributeToLocal.bat
+++
@@ -1,5 +0,0 @@
-xcopy /s /D /Y ".\bin\Release\*.exe" "E:\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.pdb" "E:\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.dll" "E:\VF_RealmPlayersData\WowLauncher"
-xcopy /s /D /Y ".\bin\Release\*.xml" "E:\VF_RealmPlayersData\WowLauncher"
-PAUSE
