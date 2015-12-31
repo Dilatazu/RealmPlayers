2bc0a3b124bb0641eff1d707ff9a30dcf99b6e89
rvandenboorn <robert.vd.boorn@gmail.com>
2015-12-31 12:18:39 +0100

    Might as welll trim the input just to be sure.

--- RealmPlayers/RaidStatsDatabase/Repositories/PurgedPlayersRepository.cs
+++ RealmPlayers/RaidStatsDatabase/Repositories/PurgedPlayersRepository.cs
@@ -14,14 +14,14 @@ namespace VF_RaidDamageDatabase.Repositories
     {
         public IEnumerable<PurgedPlayer> GetPurgedPlayers()
         {
-            string purgedPlayers = DynamicFileData.GetTextFile(Constants.RPPDbDir + "RPPWebsitePages\\PurgedPlayers.txt");
+            string purgedPlayers = DynamicFileData.GetTextFile(Constants.RDDbDir + "Variables\\PurgedPlayers.txt");
 
             var purgedPlayersCollection = (from purgedPlayer in purgedPlayers.Split('\n')
                 where purgedPlayer.Contains('|')
                 select purgedPlayer.Split('|')
                 into splitted
                 where splitted.Length.Equals(2)
-                select new PurgedPlayer(splitted[0], splitted[1]));
+                select new PurgedPlayer(splitted[0].Trim(), splitted[1].Trim()));
 
             return purgedPlayersCollection;
         }
